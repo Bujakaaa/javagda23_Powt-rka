d02c736466187b31ed1f4893af4913e70b455609
Paulina Bujała <bujala.paulina@gmail.com>
2019-03-02 11:42:37 +0100

    Zadanka

--- src/main/java/ZadComparatory/ComparatorWiek.java
+++ src/main/java/ZadComparatory/ComparatorWiek.java
@@ -6,7 +6,7 @@ public class ComparatorWiek implements Comparator<Osoba> {
     @Override
     public int compare(Osoba o1, Osoba o2) {
         if (o1.getWiek() < o2.getWiek()) {
-            return 1
+            return 1;
         } else if (o1.getWiek() == o2.getWiek()) {
             return 0;
         }
